baa3324f3c55a922d6b54741633e9262767e1cb9
angelo <angelokhan.j@gmail.com>
2022-09-07 19:40:01 -0400

    change app by adding aip_endpoints, and REPO to cloud build

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
 from flask import Flask, Response, jsonify, render_template, logging, request
+import logging
+from operator import itemgetter
+import os
+
+from flask import jsonify
+from google.cloud import aiplatform
+from google.protobuf import json_format
+from google.protobuf.struct_pb2 import Value
+import requests
+
+
+aip_client = aiplatform.gapic.PredictionServiceClient(client_options={
+    'api_endpoint': 'us-central1-aiplatform.googleapis.com'
+})
+aip_endpoint_name = f'projects/{os.environ["PROJECT_ID"]}/locations/us-central1/endpoints/{os.environ["ENDPOINT_ID"]}'
+
+
 app = Flask(__name__)
 
 
